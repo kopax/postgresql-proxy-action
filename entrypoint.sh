a2746ea6f69ee14977f803102c6d0c8ce4e6c081
Viktor Duo <v@codebros.net>
2022-07-28 16:44:04 +0100

    don't specify which host on client

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ echo "$INPUT_REMOTE_KEY" | SSH_PASS="$INPUT_REMOTE_KEY_PASS" agent-add
 # turn on strict errors
 set -eu
 
-# list interfaces
-sh -c "ifconfig"
 
-#sh -c "ssh -o StrictHostKeyChecking=no -f -N -L localhost:$INPUT_POSTGRE_PORT:$INPUT_POSTGRE_HOST:$INPUT_POSTGRE_PORT -p $INPUT_REMOTE_PORT $INPUT_REMOTE_USER@$INPUT_REMOTE_HOST $INPUT_SWITCHES"
+
+sh -c "ssh -o StrictHostKeyChecking=no -f -N -L $INPUT_POSTGRE_PORT:$INPUT_POSTGRE_HOST:$INPUT_POSTGRE_PORT -p $INPUT_REMOTE_PORT $INPUT_REMOTE_USER@$INPUT_REMOTE_HOST $INPUT_SWITCHES"
