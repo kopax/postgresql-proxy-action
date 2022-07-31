649a5ff40771d98fbeeff5e04f738cf41df386fe
Viktor Duo <v@codebros.net>
2022-07-31 13:05:49 +0100

    expose port
    5432 is PostgreSQL's default port

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,8 @@ FROM drinternet/rsync:v1.4.0
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
+LABEL org.opencontainers.image.source = "https://github.com/virbyte/postgresql-proxy-action/"
+
+EXPOSE 5432
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,4 @@ echo "$INPUT_REMOTE_KEY" | SSH_PASS="$INPUT_REMOTE_KEY_PASS" agent-add
 # turn on strict errors
 set -eu
 
-
-
 sh -c "ssh -o StrictHostKeyChecking=no -f -N -L 127.0.0.1:$INPUT_POSTGRE_PORT:$INPUT_POSTGRE_HOST:$INPUT_POSTGRE_PORT -p $INPUT_REMOTE_PORT $INPUT_REMOTE_USER@$INPUT_REMOTE_HOST $INPUT_SWITCHES"
